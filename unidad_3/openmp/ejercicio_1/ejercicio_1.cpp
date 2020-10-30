5a279caa5afc4c9ec1e3b402979c7d855bdfeabd
Javier <kachuka99@gmail.com>
2020-10-30 10:56:27 -0300

    solucion ejercicio 1 OPM - Kachuka

--- unidad_3/openmp/ejercicio_1/ejercicio_1.cpp
+++ unidad_3/openmp/ejercicio_1/ejercicio_1.cpp
@@ -1,22 +1,23 @@
 #include <omp.h>
 #include <iostream>
- 
+
 int main (){
- 
+
 int nthreads;
 int thread;
-int NuevoValor = 222;
- 
+int var = 777 ;
+
 omp_set_num_threads(10);
-#pragma omp parallel private(nthreads, thread) firstprivate(NuevoValor)
+
+#pragma omp parallel private(nthreads, thread) firstprivate(var)
   {
- 
+
   thread = omp_get_thread_num();
- 
+
   nthreads = omp_get_num_threads();
- 
-  std::cout<<"Hola Mundo soy la hebra ="<< thread <<" de "<<nthreads<<" que somos"<< " Esta es la variable nueva = "<<NuevoValor<<std::endl;
- 
+
+  std::cout<<"Hola Mundo soy la hebra ="<< thread <<" de "<<nthreads<<" que somos . el val de n es "<< var+thread <<std::endl;
+
   }
- 
+
 }
