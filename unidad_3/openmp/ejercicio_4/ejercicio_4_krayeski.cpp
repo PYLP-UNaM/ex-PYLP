80cf5a3701054d6cf4b4c7cf1fc1f1623791b34c
Tatiana Krayeski <maiakrayeski@gmail.com>
2022-11-28 22:37:37 -0300

    fixes #905

---
+++ unidad_3/openmp/ejercicio_4/ejercicio_4_krayeski.cpp
@@ -0,0 +1,50 @@
+#include <omp.h>
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+#define nthreads 4
+
+int main (){
+
+    int size = nthreads * 5;                        
+    int vectorUno[size];
+    int vectorDos[size];
+    int total = 0; 
+
+    srand(time(NULL));
+
+    // Carga de Vectores
+    for (int i = 0; i < size; i++) {
+        vectorUno[i] = rand() % 100;
+        printf("%d / ", vectorUno[i]);
+    }
+
+    printf("\nEl segundo vector es: \n");
+    for (int i = 0; i < size; i++) {
+        vectorDos[i] = rand() % 100;
+        printf("%d / ", vectorDos[i]);
+    }
+
+    omp_set_num_threads(nthreads);
+    #pragma omp parallel reduction(+:total)  
+    {
+        int thread = omp_get_thread_num();
+
+        #pragma omp for schedule(static, 5)
+
+        for (int i = 0; i < size ; i++){
+            total+= vectorUno[i] * vectorDos[i];
+            #pragma omp critical
+            {
+                printf("\n");
+                printf("Soy el hilo %d, ",thread);
+                printf("multipleque los valores de la posicion %d de los vectores, (%d x %d), ", i, vectorUno[i], vectorDos[i]);
+                printf("de momento el semi total es: %d ", total);
+                printf("\n");
+            }
+        }
+    }
+
+    printf("\nEl total es de: %d\n", total);
+}  
